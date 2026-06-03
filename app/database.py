d03c0ef056b5c1d35fe0126537783364c34d0654
Zakhar <dovidovichzakhar0@gmail.com>
2026-06-03 12:34:46 +0000

    Edit database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./bookshelf.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/bookshelf.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
